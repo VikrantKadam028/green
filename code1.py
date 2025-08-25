60770f14dfe4afe038e49fa8baae1452b7016bc1
Vikrant Kadam <vikrantkk2889@gmail.com>
2025-08-25 18:41:35 +0530

    Create code1.py

---
+++ code1.py
@@ -0,0 +1,22 @@
+
+x = "1"
+y = 1
+
+if (1 + 1) == "2":   
+    print("Math is broken!")
+else:
+    print("Condition failed...")
+
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+
+name = "Vikrant"
+msg = name + " " +
